d538e3b29f7abf0017f10a7a19ac1319b468e3de
Martin Berestovoy <martin.berestovoy@gmail.com>
2024-05-11 22:11:07 -0300

    till min 58.38

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,28 @@
-import Display from "./display"
-import ButtonPanel from "./buttonPanel"
-import Button from "./button"
-import { Component } from "react"
+import Display from "./display";
+import ButtonPanel from "./buttonPanel";
+import Button from "./button";
+import { Component } from "react";
+import Operations from "../logic/operations";
 
 class App extends Component {
-
   state = {
     total: null,
     following: null,
     operator: null,
-  }
+  };
+
+  handleClick = (buttonName) => {
+    this.setState(Operations(this.state, buttonName));
+  };
 
   render() {
     return (
-    <div>
-      <Display value={this.state.following || this.state.total || "0"} />
-      <ButtonPanel />
-    </div>
-  )
-}
+      <div>
+        <Display value={this.state.following || this.state.total || "0"} />
+        <ButtonPanel clickHandle={this.clickHandle} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/logic/operations.js
+++ src/logic/operations.js
@@ -28,6 +28,64 @@ export default function Operations (state, buttonName) {
         return {following: buttonName, total: null}
     }
 
+    if (buttonName = "/") {
+        if (state.operator && state.following) {
+            const result = operate(state.total, state.following, state.operator)
+
+            return {total: Big(result).div(Big("100")).toString(), following: null, operator: null}
+        }
+
+        if (state.following) {
+            return {folllowing: Big(state.following).div(Big("100")).toString()}
+        }
+
+        return {}
+    }
+
+    if (buttonName === ".") {
+        if (state.following) {
+            if (state.following.includes(".")) return {}
+
+            return {following: state.following + "."}
+        }
+
+        return {following: "0."}
+    }
+
+    if (buttonName === "=") {
+        if (state.following && state.operator) {
+            return {
+                total: operate(state.total, state.following, state.operator),
+                following: null,
+                operator: null,
+            }
+        }
+        
+        return {}
+    }
+
+    if (buttonName === "+/-") {
+        if (state.following) return {following: (-1 * parseFloat(state.following)).toString()}
+
+        if (state.total) return {total: (-1 * parseFloat(state.total)).toString()}
+
+        return {}
+    }
     
+    if (state.operator) {
+        return {
+            total: operate(state.following, state.total, state.operator),
+            following: null,
+            operator: buttonName,
+        }
+    }
+
+    if (!state.following) return {operator: buttonName}
+
+    return {
+        total: state.following,
+        following: null,
+        operator: buttonName,
+    }
 
 }
